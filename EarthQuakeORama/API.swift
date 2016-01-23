f6a664ffc8e8df971d17e14bce842bec957d8ac7
Dimas Lipiz <lipizdesigns@gmail.com>
2016-01-23 00:07:33 -0500

    added network check

--- EarthQuakeORama/API.swift
+++ EarthQuakeORama/API.swift
@@ -7,40 +7,50 @@
 //
 
 import UIKit
+import SystemConfiguration
+
+
 
 class API: NSObject {
     
     static func getEarthquakeInformation(completionHandler: (informationArray: NSMutableArray) -> ())  {
         
-        let session = NSURLSession.sharedSession()
-        let urlString = "http://ehp2-earthquake.wr.usgs.gov/fdsnws/event/1/query?format=geojson&limit=25"
-        let url = NSURL(string: urlString)!
-        let request = NSURLRequest(URL: url)
-        let task = session.dataTaskWithRequest(request){ data,response,downloadError in
-            
-            if let error = downloadError {
-                print("could not complete the request\(error)")
-            } else {
+        // check for network connection and send alert.
+        if NetworkReachability.isConnectedToNetwork() == true {
+            let session = NSURLSession.sharedSession()
+            let urlString = "http://ehp2-earthquake.wr.usgs.gov/fdsnws/event/1/query?format=geojson&limit=25"
+            let url = NSURL(string: urlString)!
+            let request = NSURLRequest(URL: url)
+            let task = session.dataTaskWithRequest(request){ data,response,downloadError in
                 
-                let parsedResult = try! NSJSONSerialization.JSONObjectWithData(data! , options: NSJSONReadingOptions.AllowFragments)
-                let dataDict = parsedResult as! NSDictionary
-                
-                // This holds all the information we need from the API.
-                if let result = dataDict["features"] as? NSMutableArray {
-                    
-                    completionHandler(informationArray: result)
+                if let error = downloadError {
+                    print("could not complete the request\(error)")
                 } else {
-                    print("error")
+                    
+                    let parsedResult = try! NSJSONSerialization.JSONObjectWithData(data! , options: NSJSONReadingOptions.AllowFragments)
+                    let dataDict = parsedResult as! NSDictionary
+                    
+                    // This holds all the information we need from the API.
+                    if let result = dataDict["features"] as? NSMutableArray {
+                        completionHandler(informationArray: result)
+                    } else {
+                        print("error")
+                    }
                 }
-                
             }
             
+            task.resume()
+            
+        } else {
+            // iOS 8 UIAlertView has been deprecated. Use UIAlertController instead.
+            let alert = UIAlertController(title: "No Connection", message:"There seems to be a shakeup!\n Make sure you are connected to a network", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default) { _ in })
+            let rootVC = UIApplication.sharedApplication().keyWindow?.rootViewController
+            rootVC?.presentViewController(alert, animated: true){}
+
+            
+            
         }
-        
-        task.resume()
-        
-        
+
     }
-    
-    
 }
---
+++ EarthQuakeORama/NetworkReachability.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkReachability.swift
+//  EarthQuakeORama
+//
+//  Created by Dimas retina mBP on 1/22/16.
+//  Copyright © 2016 Dimas Lipiz. All rights reserved.
+//
+
+
+import Foundation
+import SystemConfiguration
+
+public class NetworkReachability {
+    
+    class func isConnectedToNetwork() -> Bool {
+        
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, UnsafePointer($0))
+        }
+        
+        var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
+        if SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) == false {
+            return false
+        }
+        
+        let isReachable = flags == .Reachable
+        let needsConnection = flags == .ConnectionRequired
+        
+        return isReachable && !needsConnection
+        
+    }
+}
